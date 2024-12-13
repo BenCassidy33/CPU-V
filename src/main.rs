2f4551c6c5d0d5724e447029c9f00c6c361c67d6
Ben <ben.d.cassidy@gmail.com>
2024-12-13 13:43:04 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,11 @@ use crate::core::core::parse_file;
 use std::{env, fs, io::Read};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
     let mut file_buf: String = String::new();
 
-    for arg in args {
+    for arg in env::args() {
         if arg.ends_with(".asm") {
-            file_buf = fs::read_to_string(arg).expect("Could not read file. Dump");
+            file_buf = fs::read_to_string(arg).expect("Could not read file. Process Error");
         }
     }
 
